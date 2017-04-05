c668c4ba46521ca29c80dbe0ec466ebf587e8e3a
Lelouch <Conankyo@Hotmail.com>
2017-04-05 15:41:13 +0800

    Add zip , rar, folder support

--- pycomics.py
+++ pycomics.py
@@ -5,7 +5,11 @@ import sys
 import os
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QMainWindow, QTextEdit, QAction, QApplication, QFileDialog ,QLabel , QScrollArea ,QMessageBox
-from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtGui import QIcon, QPixmap, QImage 
+import zipfile
+import lzma
+import rarfile
+from io import StringIO
 
 __Title__ = 'Pycomics'
 __Version__ = 'beta 0.01'
@@ -45,6 +49,11 @@ class Pycomics(QMainWindow):
         self.OpenAction .setStatusTip('Open Files')
         self.OpenAction .triggered.connect(self.OpenFile)
 
+        self.OpenFAction = QAction(QIcon('icon' + os.sep + 'file.png'), 'Open', self)
+        self.OpenFAction .setShortcut('Ctrl+O')
+        self.OpenFAction .setStatusTip('Open Folder')
+        self.OpenFAction .triggered.connect(self.OpenFolder)
+
         self.PrevAction = QAction(QIcon('icon' + os.sep + 'file.png'), 'PreviousPage', self)
         self.PrevAction .setShortcut('Left')
         self.PrevAction .setStatusTip('PreviousPage')
@@ -69,6 +78,7 @@ class Pycomics(QMainWindow):
         menubar = self.menuBar()
         fileMenu = menubar.addMenu('&File')
         fileMenu.addAction(self.OpenAction)
+        fileMenu.addAction(self.OpenFAction)
         fileMenu.addAction(self.ExitAction)
 
         fileMenu = menubar.addMenu('&View')
@@ -80,12 +90,13 @@ class Pycomics(QMainWindow):
     def InitToolbar(self):
         toolbar = self.addToolBar('Toolbar')
         toolbar.addAction(self.OpenAction)
-        toolbar.addAction(self.ExitAction)
+        toolbar.addAction(self.OpenFAction)
         toolbar.addAction(self.FirstAction)
         toolbar.addAction(self.PrevAction)
         toolbar.addAction(self.NextAction)
         toolbar.addAction(self.LastAction)
-
+        toolbar.addAction(self.ExitAction)
+        
     def InitStatusbar(self):
         self.statusbar = self.statusBar()
         self.pathstatus = QLabel()
@@ -100,24 +111,73 @@ class Pycomics(QMainWindow):
         if path[0]:
             return os.path.realpath(path[0])
 
+    def ShowOpenFolderDialog(self):
+        path = QFileDialog.getExistingDirectory(self, 'Open Folder', '', QFileDialog.ShowDirsOnly)
+        if path:
+            return os.path.realpath(path)
+
     def OpenFile(self):
         filepath = self.ShowOpenFileDialog()
         if filepath:
             folder,fname = os.path.split(filepath)
             self.allfiles = self.GetAllFiles(folder)
             self.fileindex = self.allfiles.index(filepath)
+            self.LoadFile()
+
+    def OpenFolder(self):
+        folder = self.ShowOpenFolderDialog()
+        if folder:
+            self.allfiles = self.GetAllFiles(folder)
+            self.fileindex = 0
+            self.LoadFile()
+
+    def LoadFile(self):
+        fname = self.allfiles[self.fileindex]
+        self.archive,ext = self.iscompressed(fname)
+        if self.archive:
+            if ext == '.zip':
+                self.AcrhiveFile = zipfile.ZipFile(fname, 'r')
+                AcrhiveFileList = self.AcrhiveFile.namelist()
+                self.AllFilesInArchive = self.GetAllFilesInArchive(AcrhiveFileList)
+                self.IndexInArchive = 0
+                self.ShowImage()
+            elif ext == '.rar':
+                self.AcrhiveFile = rarfile.RarFile(fname, 'r')
+                AcrhiveFileList = self.AcrhiveFile.namelist()
+                self.AllFilesInArchive = self.GetAllFilesInArchive(AcrhiveFileList)
+                self.IndexInArchive = 0
+                self.ShowImage()
+            else:
+                self.ImageViewer.setText("Cannot load %s." % fname)
+                self.pathstatus.setText(self.allfiles[self.fileindex])
+                self.filenamestatus.setText(os.path.split(self.allfiles[self.fileindex])[1])
+                self.indexstatus.setText(str(self.fileindex+1) + '/' + str(len(self.allfiles)))
+        else:
             self.ShowImage()
 
     def ShowImage(self):
-        self.pixmap = QPixmap(self.allfiles[self.fileindex])
+        if self.archive:
+            fname = self.AllFilesInArchive[self.IndexInArchive]
+            data =self.AcrhiveFile.read(fname)
+            self.pixmap = QPixmap.fromImage(QImage.fromData(data))
+        else:
+            fname = self.allfiles[self.fileindex]
+            self.pixmap = QPixmap(fname)
+
         if self.pixmap.isNull():
-            QMessageBox.information(self, __Title__,"Cannot load %s." % fname)
-            return
-        self.ImageViewer.setPixmap(self.pixmap)
-        self.ResizeViewer()
-        self.pathstatus.setText(self.allfiles[self.fileindex])
-        self.filenamestatus.setText(os.path.split(self.allfiles[self.fileindex])[1])
-        self.indexstatus.setText(str(self.fileindex) + '/' + str(len(self.allfiles)+1))
+            self.ImageViewer.setText("Cannot load %s." % fname)
+        else:
+            self.ImageViewer.setPixmap(self.pixmap)
+            self.ResizeViewer()
+
+        if self.archive:
+            self.pathstatus.setText(self.allfiles[self.fileindex])
+            self.filenamestatus.setText(fname)
+            self.indexstatus.setText(str(self.IndexInArchive+1) + '/' + str(len(self.AllFilesInArchive)))
+        else:
+            self.pathstatus.setText(fname)
+            self.filenamestatus.setText(os.path.split(fname)[1])
+            self.indexstatus.setText(str(self.fileindex+1) + '/' + str(len(self.allfiles)))
 
     def ResizeViewer(self):
         self.ImageViewer.resize(self.pixmap.width(),self.pixmap.height())
@@ -131,6 +191,14 @@ class Pycomics(QMainWindow):
         else:
             return False
 
+    def iscompressed(self,fname):
+        compressed =['.zip','.rar','.7z']
+        ext = os.path.splitext(fname)[1]
+        if ext in compressed:
+            return True,ext
+        else:
+            return False,ext
+
     def GetAllFiles(self,folder):
         file_paths = []
         for root, directories, files in os.walk(folder):
@@ -141,23 +209,53 @@ class Pycomics(QMainWindow):
         file_paths.sort()
         return file_paths
 
+    def GetAllFilesInArchive(self,namelist):
+        file_paths = []
+        for files in namelist:
+                if self.supportfile(files):
+                    file_paths.append(files)
+        file_paths.sort()
+        return file_paths
+
     def PrevPage(self):
-        if self.fileindex>0:
-            self.fileindex -=1
-            self.ShowImage()
+        if self.archive:
+            if self.IndexInArchive>0:
+                self.IndexInArchive -=1
+                self.ShowImage()
+        else:
+            if self.fileindex>0:
+                self.fileindex -=1
+                self.LoadFile()
 
     def NextPage(self):
-        if self.fileindex < len(self.allfiles)-1:
-            self.fileindex +=1
-            self.ShowImage()
+        if self.archive:
+            if self.IndexInArchive < len(self.AllFilesInArchive)-1:
+                self.IndexInArchive +=1
+                self.ShowImage()
+            else:
+                if self.fileindex < len(self.allfiles)-1:
+                    self.fileindex +=1
+                    self.LoadFile()
+        else:
+            if self.fileindex < len(self.allfiles)-1:
+                self.fileindex +=1
+                self.LoadFile()
 
     def FirstPage(self):
-        self.fileindex = 0
-        self.ShowImage()
+        if self.archive:
+            self.IndexInArchive = 0
+            self.ShowImage()
+        else:
+            self.fileindex = 0
+            self.LoadFile()
 
     def LastPage(self):
-        self.fileindex = len(self.allfiles)-1
-        self.ShowImage()
+        if self.archive:
+            self.IndexInArchive = len(self.AllFilesInArchive)-1
+            self.ShowImage()
+        else:
+            self.fileindex = len(self.allfiles)-1
+            self.LoadFile()
 
 
 if __name__ == '__main__':
