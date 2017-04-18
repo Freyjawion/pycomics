85c2cacd5eddfdcf322ba42eee2e92d85ee4f49e
Lelouch <Conankyo@Hotmail.com>
2017-04-18 15:05:46 +0800

    Add Pwd Viewer

--- pycomics.py
+++ pycomics.py
@@ -3,9 +3,10 @@
 
 import sys
 import os
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QMainWindow, QTextEdit, QAction, QApplication, QFileDialog, QLabel, QScrollArea, QMessageBox, QListView, QDialog
-from PyQt5.QtGui import QIcon, QPixmap, QImage 
+from PyQt5.QtCore import Qt, QItemSelectionModel
+from PyQt5.QtWidgets import QMainWindow, QTextEdit, QAction, QApplication, QFileDialog, QLabel, QLineEdit
+from PyQt5.QtWidgets import QScrollArea, QMessageBox, QListView, QDialog, QHBoxLayout, QVBoxLayout, QPushButton
+from PyQt5.QtGui import QIcon, QPixmap, QImage, QStandardItem, QStandardItemModel 
 import zipfile
 import lzma
 import rarfile
@@ -302,6 +303,8 @@ class Pycomics(QMainWindow):
             self.LoadFile()
 
     def LoadPwd(self):
+        if not os.path.exists(r'password.pwd'):
+            open("password.pwd","wb").close()
         pwdf = open('password.pwd','rb')
         self.PwdList = pwdf.read().splitlines()
         pwdf.close()
@@ -334,25 +337,86 @@ class PwdManager(QDialog):
     def __init__(self):
         super().__init__()
         self.setWindowFlags(Qt.WindowCloseButtonHint | Qt.WindowTitleHint)
+        self.PwdViewer = QListView()
+        self.PwdViewer.clicked.connect(self.GetPwd)
+        self.AddButton = QPushButton('Add')
+        self.AddButton.clicked.connect(self.AddPwd)
+        self.DelButton = QPushButton('Delete')
+        self.DelButton.clicked.connect(self.DelPwd)
+        self.PwdBox = QLineEdit()
+        self.PwdBox.textEdited.connect(self.PwdChanged)
+
 
     def showEvent(self,event):
         self.setWindowTitle('Password Manager')
         self.setFixedSize(400,300)
-        self.PwdViewer = QListView()
-        self.PwdViewer.resize(100,300)
-        self.scrollArea = QScrollArea(self)
-        self.scrollArea.setWidget(self.PwdViewer)
-        self.scrollArea.setAlignment(Qt.AlignCenter)
 
-
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.AddButton)
+        vbox.addWidget(self.DelButton)
+        vbox.addStretch(1)     
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.PwdViewer)
+        hbox.addLayout(vbox)
+        MainBox = QVBoxLayout()
+        MainBox.addWidget(self.PwdBox)
+        MainBox.addLayout(hbox)
+        self.setLayout(MainBox)
         self.center()
+        self.LoadPwdToList()
+
+
+    def closeEvent(self,event):
+        pass
+
 
     def center(self):
         frameGm = self.frameGeometry()
         screen = QApplication.desktop().screenNumber(QApplication.desktop().cursor().pos())
         centerPoint = QApplication.desktop().screenGeometry(screen).center()
         frameGm.moveCenter(centerPoint)
-        self.move(frameGm.topLeft()) 
+        self.move(frameGm.topLeft())
+
+    def LoadPwdToList(self):
+        if not os.path.exists(r'password.pwd'):
+            open("password.pwd","wb").close()
+        pwdf = open('password.pwd','r')
+        pwdlist = pwdf.read().splitlines()
+        pwdf.close()
+        self.Model = QStandardItemModel(self.PwdViewer)
+        for pwd in pwdlist:
+            item = QStandardItem(pwd)
+            item.setEditable(False)
+            self.Model.appendRow(item)
+        self.PwdViewer.setModel(self.Model)
+        self.PwdViewer.setCurrentIndex(self.Model.index(0,0))
+        self.GetPwd(self.PwdViewer.currentIndex())
+    
+    def GetPwd(self,index):
+        self.PwdBox.setText(self.Model.data(index))
+
+    def PwdChanged(self):
+         self.Model.setData(self.PwdViewer.currentIndex(),self.PwdBox.text())
+         if self.PwdBox.text() == '':
+            self.DelPwd()
+
+    def DelPwd(self):
+        self.Model.removeRow(self.PwdViewer.currentIndex().row())
+        self.GetPwd(self.PwdViewer.currentIndex())
+        
+    def AddPwd(self):
+        item = QStandardItem()
+        item.setEditable(False)
+        self.Model.appendRow(item)
+        self.PwdViewer.setCurrentIndex(self.Model.index(self.Model.rowCount(),0))
+        self.GetPwd(self.PwdViewer.currentIndex())
+
+
+
+
+
+
+
 
 def main():
     app = QApplication(sys.argv)
